2a880285755b258e7221ae236ae46831abde5ddd
Misutes <misutes@list.ru>
2020-04-30 21:32:43 +0300

    broken prediction

--- Predictor.py
+++ Predictor.py
@@ -1,8 +1,24 @@
+from sklearn.neural_network import MLPRegressor
+import matplotlib.pyplot as plt
+from DataFrame_org import *
 
 
-def main():
-    pass
+def machin_learning(dataframe):
+    MLP = MLPRegressor()
+    X, Y = dataframe[:2]
+    MLP.fit(X, Y)
+    return MLP
 
 
-if __name__ == '__main__':
-    main()
+def visualization():
+    model = machin_learning(dataframe_formation(learning_date()))
+    X_test, Y_test = dataframe_formation(learning_date())[2:]
+    prediction = model.predict(X_test.head(1))
+    real_rates = Y_test.iloc[0]
+    plt.plot(future_days_columns, prediction[0], label='prediction')
+    plt.plot(future_days_columns, real_rates, label='real')
+    plt.legend()
+    plt.show()
+
+
+visualization()
