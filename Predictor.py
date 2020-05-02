cd298c1aa354c4375fb94df47b51d4c3ac6daa24
Misutes <misutes@list.ru>
2020-05-02 21:15:08 +0300

    new data for predictor

--- Predictor.py
+++ Predictor.py
@@ -1,26 +1,24 @@
 from sklearn.neural_network import MLPRegressor
 import matplotlib.pyplot as plt
-from DataFrame_org import learning_dataframe, future_days_columns
+from DataFrame_org import learning_dataframe, days_columns
 
 
 finally_dataframe = learning_dataframe()
 
 
 def building_model(max_iter, hidden_layer_sizes):
-    MLP = MLPRegressor(max_iter=max_iter, hidden_layer_sizes=hidden_layer_sizes)
-    X, Y = finally_dataframe[:2]
-    MLP.fit(X, Y)
+    MLP = MLPRegressor(max_iter=max_iter, hidden_layer_sizes=hidden_layer_sizes, random_state=42)
+    x_train, y_train = finally_dataframe[:2]
+    MLP.fit(x_train, y_train)
     return MLP
 
 
-def visualization(ml_model):
-    model = ml_model
-    X_test, Y_test = finally_dataframe[2:]
-    prediction = model.predict([X_test.iloc[0]])
-    real_rates = Y_test.iloc[0]
-    plt.plot(future_days_columns, prediction[0], label='prediction')
-    plt.plot(future_days_columns, real_rates, label='real')
+def visualization(network):
+    model = network
+    x_test, y_test = finally_dataframe[2:]
+    prediction = model.predict([x_test[2]])
+    real_rates = y_test[2]
+    plt.plot(real_rates, label='real')
+    plt.plot(prediction[0], label='prediction')
     plt.legend()
     plt.show()
-
-
